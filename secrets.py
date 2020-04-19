be45a08369ef261801c176919fe829d3fd0e7f90
Dominika <DCiesielczuk@gmail.com>
2020-04-19 22:13:02 +0200

    index error to fix

---
+++ secrets.py
@@ -0,0 +1,2 @@
+spotify_token = "BQCoga7sJ1vwATC5C_E_qGx5IlkUstS4Prx8_Smxt8d3Th1LYKbOYxAN0DbIwc8y3OleJyHx4AIfZW0AHLSaXEkvoCkoiGMtH_tvL1zzQvLEVbiRLT9lkgit-P0jeYKItqlv04ECJ8LFCHJDQviGfl3e"
+spotify_user_id = "dominika.ciesielczuk"
